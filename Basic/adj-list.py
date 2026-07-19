eb3bef38859a492661cd49fbad7913c52f6410d0
ayesha2104 <ayeshamohapatra12@gmail.com>
2026-07-19 22:27:20 +0530

    Question 0.1.1

--- Basic/adj-list.py
+++ Basic/adj-list.py
@@ -1,16 +1,21 @@
 from collections import defaultdict
 
-def adjList(n,edges):
-    
-    graph=defaultdict(list)
-    #Undirected,unweighted
-    for u,v in edges:
+def adjList(n, edges):
+    graph = defaultdict(list)
+
+    for i in range(n):
+        graph[i]
+
+    for u, v in edges:
         graph[u].append(v)
         graph[v].append(u)
-        
-    print(graph)
 
-edges=[(1,0),(0,2),(2,3),(3,1)]
-n=4
+    return graph
 
-adjList(n,edges)
+graph = adjList(n, edges)
+
+print(graph)
+
+# Later
+bfs(graph, 0)
+dfs(graph, 0)
